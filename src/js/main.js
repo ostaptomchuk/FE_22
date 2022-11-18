79709f795839439c1bf5cde99916c753ab78eecf
Ostap Tomchuk <ostaptomchuk@gmail.com>
2022-11-18 19:07:10 +0200

    simple landing with using svg format and sprites

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector('.js-burger-btn');
-const header = document.querySelector('.header-js');
-const searchBtn = document.querySelector('.js-search-btn');
-
-burger.addEventListener('click', () => {
-    header.classList.toggle('active');
-});
-
